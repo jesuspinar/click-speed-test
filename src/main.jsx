a03bf3d993889463f13986e8f0808386a5bbcaad
Jesus Pinar <not@email.com>
2024-09-14 20:11:43 +0200

    Add dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import theme from "./theme/theme.js";
+import { CssBaseline, CssVarsProvider } from "@mui/joy";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<CssVarsProvider theme={theme} defaultMode="system" modeStorageKey="preferred-mode">
+			<CssBaseline />
+			<App />
+		</CssVarsProvider>
+	</React.StrictMode>
+);
---
+++ src/theme/theme.js
@@ -0,0 +1,5 @@
+import { extendTheme } from "@mui/joy/styles";
+
+const theme = extendTheme({ colorSchemeSelector: "dark" });
+
+export default theme;
